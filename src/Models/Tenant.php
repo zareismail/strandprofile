5f9702f7b3fad2aa527abe4bf6222f6cff1b731c
اسماعیل زارع <zarehesmaiel@gmail.com>
2021-02-10 19:02:42 +0330

    Update Tenant.php

--- src/Models/Tenant.php
+++ src/Models/Tenant.php
@@ -5,6 +5,11 @@ namespace Zareismail\Strandprofile\Models;
 
 class Tenant extends User 
 { 
+	/**
+	 * Get the related references.
+	 * 
+	 * @return \Illuminate\Database\Eloquent\Relations\HasOneOrMany
+	 */
 	public function references()
 	{
 		return $this->hasMany(StrandprofileReference::class, 'auth_id');
